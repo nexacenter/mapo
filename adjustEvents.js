0e4eab692021f8a8823891c2265b0b3cf29a94c5
alemela <alessiom92@gmail.com>
2015-10-21 17:53:18 +0200

    changed URI

--- adjustEvents.js
+++ adjustEvents.js
@@ -77,6 +77,7 @@ eventJson.forEach(function (e) {
     if (e.foto != undefined && e.foto !== "")
         e.foto = e.urlComune + "/"+ e.foto;
     e.id = "http://mapo.nexacenter.org/id/eventi/" + e.id;
+    e.categoria = "http://mapo.nexacenter.org/id/eventi/categorie/" + e.categoria;
     eventArray.push(e);
 });
 
